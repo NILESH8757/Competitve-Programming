95e1ee22b6bae501dbc62e00be5048d5eeb290a8
NILESH <nilesh8757@gmail.com>
2018-05-01 01:01:31 +0530

    Create triplet_vector.cpp

---
+++ triplet_vector.cpp
@@ -0,0 +1,25 @@
+/*
+I have several integer elements like this:
+(3 9 1), (1 5 2), (2 8 3), (1 4 4), (1 6 5), (1 5 6).
+
+After sorting it should look like this:
+(1 4 4), (1 5 2), (1 5 6), (1 6 5), (2 8 3), (3 9 1).
+*/
+
+// code-
+
+vector<array<int,3>> vec;
+int n,x,y,z;
+cin>>n;
+rep(i,1,n)
+{
+    cin>>x>>y>>z;
+    vec.pb({x,y,z});
+}
+
+sort(vec.begin(), vec.end());
+
+for(auto v: vec)
+{
+    cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<endl;
+}

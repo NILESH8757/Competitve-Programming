acc0a2ecdff11f4b3b1a1a043d59c39ce413b2a0
Nilesh Kumar <nilesh8757@gmail.com>
2019-03-28 17:39:32 +0530

    Create dearrangement.cpp

---
+++ Favourite Problems/dearrangement.cpp
@@ -0,0 +1,47 @@
+//Dearrange given unsorted array so that, no element remains in their original place
+//Blog: https://codeforces.com/blog/entry/66176
+//Problem: https://www.codechef.com/problems/MARCAPS
+
+int main()
+{ 
+ 
+  int t,n,x;
+  cin>>t;
+
+  while(t--)
+  {
+    cin>>n;
+    
+    int mx=0;
+    map<int,int> mp;
+    vector<int> res(n);
+    vector<pair<int,int> > a;
+
+    for(int i=0;i<n;i++)
+    {
+      cin>>x;
+      a.push_back({x,i});
+      mp[x]++;
+      mx=max(mx,mp[x]);
+    }
+    
+    if(2*mx>n)
+    {
+      cout<<"No"<<endl;
+      continue;
+    }
+
+    sort(a.begin(), a.end());
+    
+    for(int i=0;i<a.size();i++)
+    {
+      int pos=(i+mx)%n;
+      res[a[pos].second]=a[i].first;  
+    }
+
+    cout<<"Yes\n";
+    for(int i=0;i<n;i++)
+      cout<<res[i]<<' ';
+
+    cout<<endl;
+  }

25fe787a9eebb5fcc73ac318c154c41a3beeb913
Nilesh Kumar <nilesh8757@gmail.com>
2019-04-20 16:03:01 +0530

    Update modExp.cpp

--- modExp.cpp
+++ modExp.cpp
@@ -3,26 +3,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-const ll MOD=1e9+7;
-
-ll fast_exp(ll base, ll exp,ll M) 
+int modexp(int base, int exp,int M)
 {
-    ll res=1;
-    while(exp>0) {
-       if(exp%2==1) res=(res*base)%M;
-       base=(base*base)%M;
-       exp/=2;
+    //(base ^ exp ) % M
+    int res = 1LL;
+    while(exp) 
+    {
+       if(exp & 1) 
+           res = (res * base) % M;
+       base = (base * base) % M;
+       exp /= 2;
     }
-    return res%M;
+    return res % M;
 }
+
+//modular inverse
+int modInverse(int a, int m) 
+{ 
+ // (a ^ -1) % m, m must be prime
+ return modexp(a, m-2, m);   
+} 
 
 int main() 
 {
-    ll n,tem,ans;
-    cin>>n;
-    tem=fast_exp(3,n,MOD-1);
-    ans=fast_exp(3,tem,MOD);
-    cout<<ans;
+    int n, tem, ans;
+    cin >> n;
+    tem = fast_exp(3, n, MOD - 1);
+    ans = fast_exp(3, tem, MOD);
+    cout << ans;
     return 0;
 }

ab277bc570df9132633ab4a0d564be5dcb203a11
Nilesh Kumar <nilesh8757@gmail.com>
2019-04-27 10:37:03 +0530

    Create N_sequence.cpp

---
+++ Favourite Problems/N_sequence.cpp
@@ -0,0 +1,71 @@
+/*
+  using integers from 1 to K, generate N sequnces each of which is of length D such that no two sequence is identical.
+  Print -1 if not possible.
+  
+  source: https://codeforces.com/problemset/problem/459/C
+*/
+
+
+#include <bits/stdc++.h>
+ using namespace std;
+const int MAX_N = 1015;
+int res[MAX_N][MAX_N];
+
+int possible(int people, int buses, int days)
+{
+    //Check if k^d > n
+    for(int power = 1, d = 0; d <= days; power *= buses, d++)
+    {
+        if(power >= people)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+    #endif 
+
+    int N, K, D;
+
+    scanf("%d %d %d", &N, &K, &D);
+
+    if(!possible(N, K, D))
+    {
+        printf("-1\n");
+        return 0;
+    }
+
+    for(int p = 0; p < N; p++)
+    {
+        for(int d = 0; d < D; d++)
+        {
+            res[p][d] = (p == 0 ? 0 : res[p - 1][d]);
+        }
+
+        for(int d = D - 1; d >= 0; d--)
+        {
+            res[p][d]++;
+            res[p][d] %= K;
+
+            if(res[p][d])
+                break;
+        }
+
+    }
+    
+    for(int d = 0; d < D; d++)
+    {
+        for(int p = 0; p < N; p++)
+        {
+            printf("%d ", res[p][d] + 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

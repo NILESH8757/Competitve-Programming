28fba9d930fc60a34f2958ff96a55fc8b4b5d818
Nilesh Kumar <nilesh8757@gmail.com>
2019-09-24 21:03:56 +0530

    Update SCC(Kosaraju's algorithm).cpp

--- SCC(Kosaraju's algorithm).cpp
+++ SCC(Kosaraju's algorithm).cpp
@@ -1,6 +1,10 @@
 //blog : https://cp-algorithms.com/graph/strongly-connected-components.html
+
 // problem 1: https://www.spoj.com/problems/TFRIENDS/  [ simply getting count of SCCs in the graph]
 // solution 1 : http://ideone.com/Sr8lSc 
 
 // problem 2 : https://www.codechef.com/problems/MCO16405 [creating condensed graph & processing answer from it ]
 // solution 2 : https://www.codechef.com/viewsolution/26765573 
+
+// problem 3 : https://www.spoj.com/problems/BREAK/
+// solution 3 : http://ideone.com/QoWS56

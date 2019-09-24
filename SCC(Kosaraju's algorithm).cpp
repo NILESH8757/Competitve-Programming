b1ffd68997e735dd6e19c2be94e708c676c50838
Nilesh Kumar <nilesh8757@gmail.com>
2019-09-24 15:13:57 +0530

    Update SCC(Kosaraju's algorithm).cpp

--- SCC(Kosaraju's algorithm).cpp
+++ SCC(Kosaraju's algorithm).cpp
@@ -1,2 +1,3 @@
+//blog : https://cp-algorithms.com/graph/strongly-connected-components.html
 // problem 1: https://www.spoj.com/problems/TFRIENDS/
 // solution 1 : http://ideone.com/Sr8lSc [in O(V+E) time using Kosaraju's algorithm]

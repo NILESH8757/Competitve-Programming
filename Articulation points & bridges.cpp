95ab7e3d9694741f369f444e8addd10fc6b145ff
Nilesh Kumar <nilesh8757@gmail.com>
2019-09-25 17:02:39 +0530

    Update Articulation points & bridges.cpp

--- Articulation points & bridges.cpp
+++ Articulation points & bridges.cpp
@@ -1,7 +1,7 @@
 // resource 1 : https://cp-algorithms.com/graph/cutpoints.html
 // resource 2 : https://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf
 
-// problme 1 : https://www.spoj.com/problems/SUBMERGE/ [straightforward AP]
+// problme 1 : https://www.spoj.com/problems/SUBMERGE/ [straightforward Articulation points problem ]
 // solution 1 : http://ideone.com/ZQDphD
 
 //-----------------------------------------------------------------------------------------

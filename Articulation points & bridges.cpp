8cd6ea9e9fdaa03f914c2ed792f88a8f6ac8bea2
Nilesh Kumar <nilesh8757@gmail.com>
2019-09-25 17:01:00 +0530

    Update and rename Articulation points.cpp to Articulation points & bridges.cpp

---
+++ Articulation points & bridges.cpp
@@ -0,0 +1,11 @@
+// resource 1 : https://cp-algorithms.com/graph/cutpoints.html
+// resource 2 : https://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf
+
+// problme 1 : https://www.spoj.com/problems/SUBMERGE/ [straightforward AP]
+// solution 1 : http://ideone.com/ZQDphD
+
+//-----------------------------------------------------------------------------------------
+
+// resource 3 : https://cp-algorithms.com/graph/bridge-searching.html
+// problem 2 : https://www.spoj.com/problems/EC_P/ [ straightforward bridge problem ]
+// solution 2 : http://ideone.com/o9S4w5
--- Articulation points.cpp
+++
@@ -1,5 +0,0 @@
-// resource 1 : https://cp-algorithms.com/graph/cutpoints.html
-// resource 2 : https://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf
-
-// problme 1 : https://www.spoj.com/problems/SUBMERGE/ [straightforward AP]
-// solution 1 : http://ideone.com/ZQDphD

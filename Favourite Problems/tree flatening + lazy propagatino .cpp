132212f2867f6df33d79c3f06b97db5be8d8f790
Nilesh Kumar <nilesh8757@gmail.com>
2020-10-06 22:14:19 +0530

    Create tree flatening + lazy propagatino .cpp

---
+++ Favourite Problems/tree flatening + lazy propagatino .cpp
@@ -0,0 +1,5 @@
+// Flatten tree and build segment tree to assign val in range and count distinct element in range,  1 <= val <= 60
+
+// https://codeforces.com/contest/620/problem/E
+// Solution : https://codeforces.com/contest/620/submission/94865218
+

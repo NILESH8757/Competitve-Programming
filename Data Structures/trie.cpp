b5516135ce6088a8c8632ea4c316ab734edc3c35
Nilesh Kumar <nilesh8757@gmail.com>
2023-01-14 19:55:05 +0530

    Update trie.cpp

--- Data Structures/trie.cpp
+++ Data Structures/trie.cpp
@@ -53,3 +53,57 @@ int maxxor(int x)
 // problem : https://codeforces.com/problemset/problem/706/D
 // Probme 2 : https://www.codechef.com/CDCRFT14/problems/SUBBXOR [ count of subarrays with xor less than k ]
 // Solution 2 : https://www.codechef.com/viewsolution/27949729
+
+// Problem 3: https://www.spoj.com/problems/ADAINDEX/
+// Solution 3: 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef struct data{
+    data* next[26];
+    int cnt = 0;
+} trie;
+trie* head;
+ 
+void trie_insert(string &s){
+    trie* cur = head;
+    for(char c: s){
+        int idx = c - 'a';
+        if(!cur->next[idx])
+			cur->next[idx] = new trie();
+		cur = cur->next[idx];
+		cur->cnt++;
+    }
+}
+ 
+int trie_count_prefix(string &s){
+    trie* cur = head;
+    int ans = INT_MAX;
+    for(char c: s){
+        int idx = c - 'a';
+        if(cur->next[idx] != NULL){
+          cur = cur->next[idx];
+          ans = min(ans, cur->cnt);
+        } else {
+            ans = 0;
+            break;
+        }
+    }
+    return ans;
+}
+int main(){
+   int n, q;
+   string s;
+   cin >> n >> q;
+   head = new trie();
+   for(int i = 0; i < n; i++){
+       cin >> s;
+       trie_insert(s);
+   }
+   while(q--){
+       cin >> s;
+       int ans = trie_count_prefix(s);
+       cout << ans << "\n";
+   }
+   return 0;
+} 

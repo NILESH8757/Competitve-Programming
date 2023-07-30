9365bdee3b271a2e16734e43601d46fc7a4c9094
Nilesh Kumar <nilesh8757@gmail.com>
2023-07-30 19:34:43 +0530

    Handling 0 cleverly

--- digit_dp.cpp
+++ digit_dp.cpp
@@ -1,70 +1,69 @@
 // Problem1: https://leetcode.com/problems/count-stepping-numbers-in-range
-// Problem1's solution:
+// Problem1's solution(observe how cleverly 0 is handled with 10 as 'last' flag):
 class Solution {
 public:
-  int dp[102][2][12][2], n;
+
+  int dp[102][2][12], n;
   string s;
   const int mod = 1e9 + 7;
-  // not including 0 in answer as constraints > 0
-  int countSteppingNumbers(int idx, int smaller, int last, int is_zero){
-      if(idx == n) {
-         return is_zero ? 0: 1;
-      }
-      int &ans = dp[idx][smaller][last][is_zero];
-      if(ans != -1)
-          return ans;
+    
+    int countSteppingNumbers(int idx, int tight, int last){
+        if(idx == n) {
+           return 1;
+        }
+        int &ans = dp[idx][tight][last];
+        if(ans != -1)
+            return ans;
 
-      int limit = 9;
-      if(smaller)
-          limit = s[idx] - '0';
+        ans = 0;
+        int current_digit = (s[idx] - '0');
+        int higher_digit = last + 1;
+        int lower_digit = last - 1;
 
-      ans = 0;
-
-      for(int i = 0; i <= limit; i++) {
-          
-          int next_smaller = smaller;
-          if(smaller && i < limit)
-            next_smaller = 0;
-          int will_be_zero = is_zero;
-          if(is_zero && i > 0)
-            will_be_zero = 0;
-
-          if(is_zero){
-              ans += countSteppingNumbers(idx + 1, next_smaller, i, will_be_zero);
+        int limit = tight ? current_digit : 9;
+        
+        if(last == 10){
+            for(int i = 0; i <= limit; i++){
+               ans += countSteppingNumbers(idx + 1, tight & (i == limit), i == 0 ? 10: i);
+               ans %= mod;
+            }
+        } else{
+           if(higher_digit <= limit){
+              ans += countSteppingNumbers(idx + 1, tight & (higher_digit == limit), higher_digit);
               ans %= mod;
-          } else{
-              if(abs(i - last) == 1){
-                ans += countSteppingNumbers(idx + 1, next_smaller, i, will_be_zero);
-                ans %= mod;
-              }
-          }
-      }
-      return ans;
-  }
-  int countSteppingNumbers(string low, string high) {
-       s = high;
-       n = s.size();
-       memset(dp, -1, sizeof(dp));
-       int ans = countSteppingNumbers(0, 1, 0, 1);
+           }
+           if(lower_digit >= 0 && lower_digit <= limit){
+              ans += countSteppingNumbers(idx + 1, tight & (lower_digit == limit), lower_digit);
+              ans %= mod;
+           }
+        }
+        
+        return ans;
+    }
+    int countSteppingNumbers(string low, string high) {
+         s = high;
+         n = s.size();
+         memset(dp, -1, sizeof(dp));
+         int ans = countSteppingNumbers(0, 1, 10); // 10 is flag for leading zero
 
-       s = low;
-       n = s.size();
-       memset(dp, -1, sizeof(dp));
-       ans = (ans - countSteppingNumbers(0, 1, 0, 1) + mod) % mod;
+         s = low;
+         n = s.size();
+         memset(dp, -1, sizeof(dp));
+         ans = (ans - countSteppingNumbers(0, 1, 10) + mod) % mod;
 
-       bool check_low = true;
-       for(int i = 1; i < low.size(); i++){
-           if(abs(low[i] - low[i - 1]) != 1)
-              check_low = false;
-       }
-       if(check_low){
-           ans++;
-           ans %= mod;
-       }
-       return ans;
-  }
+         bool check_low = true;
+         for(int i = 1; i < low.size(); i++){
+             if(abs(low[i] - low[i - 1]) != 1)
+                check_low = false;
+         }
+         if(check_low){
+             ans++;
+             ans %= mod;
+         }
+         return ans;
+    }
 };
-//Problem2 (same as problem1) includes 0 (but in adhoc manner):  https://cses.fi/problemset/task/2220/
-//Problem3 (without use of zero flag): https://atcoder.jp/contests/dp/tasks/dp_s 
-//Problem4 (without use of zero flag): https://leetcode.com/problems/count-of-integers 
+//Problem2 (similar as problem1):  https://cses.fi/problemset/task/2220/ [solution: https://cses.fi/problemset/result/6643437/]
+//Problem3 : https://atcoder.jp/contests/dp/tasks/dp_s 
+//Problem4 : https://leetcode.com/problems/count-of-integers 
 //Problem5: (digit dp with bitmask): https://leetcode.com/problems/count-special-integers

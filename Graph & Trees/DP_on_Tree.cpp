cccb1fc3f78ea99dfa0729941218fbc0bba518a8
Nilesh Kumar <nilesh8757@gmail.com>
2023-08-07 17:16:57 +0530

    Added more problems

--- Graph & Trees/DP_on_Tree.cpp
+++ Graph & Trees/DP_on_Tree.cpp
@@ -8,3 +8,9 @@ solution :https://cses.fi/paste/fefa4fe0869303b46665c8/
 # Maximum Tree Matching
 Problem: https://cses.fi/problemset/task/1130/
 Solutions: https://cses.fi/paste/25f25262e0d3bb3f666bb5/
+
+# LCA + Prefix sum (count how many paths goes through each nodes)
+Problem: https://cses.fi/problemset/result/6713218/ [Counting Paths]
+Solution: https://cses.fi/paste/396eda22aee3530c666f82/  
+
+

881dfb16683c1f6035cfa2087431cb77ad3933d8
Nilesh Kumar <nilesh8757@gmail.com>
2023-08-07 15:46:35 +0530

    Added tree matching problem

--- Graph & Trees/DP_on_Tree.cpp
+++ Graph & Trees/DP_on_Tree.cpp
@@ -1,6 +1,10 @@
-Re-rooting technique problemm:
+# Re-rooting technique problemm:
 Problem1: https://cses.fi/problemset/task/1132 [Tree Distances I]
 Solution: https://cses.fi/paste/0dd73fec10f7e9cf66673a/
 
 Problem2 : https://cses.fi/problemset/task/1133/ [Tree Distances II]
 solution :https://cses.fi/paste/fefa4fe0869303b46665c8/ 
+
+# Maximum Tree Matching
+Problem: https://cses.fi/problemset/task/1130/
+Solutions: https://cses.fi/paste/25f25262e0d3bb3f666bb5/

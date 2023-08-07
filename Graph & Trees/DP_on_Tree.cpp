5bfc65a169837129f46e145adca57bcb9ec7422a
Nilesh Kumar <nilesh8757@gmail.com>
2023-08-07 13:36:56 +0530

    Added more problems

--- Graph & Trees/DP_on_Tree.cpp
+++ Graph & Trees/DP_on_Tree.cpp
@@ -1,3 +1,6 @@
 Re-rooting technique problemm:
-Problem1 : https://cses.fi/problemset/task/1133/ [Tree Distances II]
+Problem1: https://cses.fi/problemset/task/1132 [Tree Distances I]
+Solution: https://cses.fi/paste/0dd73fec10f7e9cf66673a/
+
+Problem2 : https://cses.fi/problemset/task/1133/ [Tree Distances II]
 solution :https://cses.fi/paste/fefa4fe0869303b46665c8/ 

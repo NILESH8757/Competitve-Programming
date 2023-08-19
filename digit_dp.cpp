e5592ee1b471ff9ab1ea4b6698d4789d6f81f044
Nilesh Kumar <nilesh8757@gmail.com>
2023-08-19 22:34:02 +0530

    Added new problem

--- digit_dp.cpp
+++ digit_dp.cpp
@@ -1,69 +1,53 @@
-// Problem1: https://leetcode.com/problems/count-stepping-numbers-in-range
-// Problem1's solution(observe how cleverly 0 is handled with 10 as 'last' flag):
+// Problem1: https://leetcode.com/problems/number-of-beautiful-integers-in-the-range
 class Solution {
 public:
-
-  int dp[102][2][12], n;
-  string s;
-  const int mod = 1e9 + 7;
+    int dp[11][2][11][11][21][2];
+    string s;
+    int n, k;
+    int countBeautifulNumbers(int idx, int tight, int evens, int odds, int rem, int zero){
     
-    int countSteppingNumbers(int idx, int tight, int last){
-        if(idx == n) {
-           return 1;
+        if(idx >= s.size()) {
+           return (odds == evens) && (rem == 0) && (zero == 0);
         }
-        int &ans = dp[idx][tight][last];
+
+        int &ans = dp[idx][tight][evens][odds][rem][zero];
         if(ans != -1)
             return ans;
 
+        int limit = tight ? (s[idx] - '0') : 9;
+        
         ans = 0;
-        int current_digit = (s[idx] - '0');
-        int higher_digit = last + 1;
-        int lower_digit = last - 1;
-
-        int limit = tight ? current_digit : 9;
         
-        if(last == 10){
-            for(int i = 0; i <= limit; i++){
-               ans += countSteppingNumbers(idx + 1, tight & (i == limit), i == 0 ? 10: i);
-               ans %= mod;
+        for(int i = 0; i <= limit; i++){
+            int new_tight = tight & (i == limit);
+            int new_zero = zero ? i == 0 : 0;
+            int new_evens = evens;
+            int new_odds = odds;
+            int new_rem = ((rem * 10) + i) % k;
+            if(new_zero == 0){
+                new_evens = evens + (i % 2 == 0);
+                new_odds = odds + (i & 1);
             }
-        } else{
-           if(higher_digit <= limit){
-              ans += countSteppingNumbers(idx + 1, tight & (higher_digit == limit), higher_digit);
-              ans %= mod;
-           }
-           if(lower_digit >= 0 && lower_digit <= limit){
-              ans += countSteppingNumbers(idx + 1, tight & (lower_digit == limit), lower_digit);
-              ans %= mod;
-           }
+            ans += countBeautifulNumbers(idx + 1, new_tight, 
+                    new_evens, new_odds, new_rem, new_zero);
         }
-        
         return ans;
     }
-    int countSteppingNumbers(string low, string high) {
-         s = high;
-         n = s.size();
-         memset(dp, -1, sizeof(dp));
-         int ans = countSteppingNumbers(0, 1, 10); // 10 is flag for leading zero
-
-         s = low;
-         n = s.size();
-         memset(dp, -1, sizeof(dp));
-         ans = (ans - countSteppingNumbers(0, 1, 10) + mod) % mod;
-
-         bool check_low = true;
-         for(int i = 1; i < low.size(); i++){
-             if(abs(low[i] - low[i - 1]) != 1)
-                check_low = false;
-         }
-         if(check_low){
-             ans++;
-             ans %= mod;
-         }
-         return ans;
+    int numberOfBeautifulIntegers(int low, int high, int k) {
+        this->k = k;
+        memset(dp, -1, sizeof(dp));
+        s = to_string(high);
+        n = s.size();
+        int high_ans = countBeautifulNumbers(0, 1, 0, 0, 0, 1);
+        memset(dp, -1, sizeof(dp));
+        s = to_string(low - 1);
+        n = s.size();
+        int low_ans = countBeautifulNumbers(0, 1, 0, 0, 0, 1);
+        int ans = high_ans - low_ans;
+        return ans;
     }
 };
-//Problem2 (similar as problem1):  https://cses.fi/problemset/task/2220/ [solution: https://cses.fi/problemset/result/6643437/]
+//Problem2 :  https://cses.fi/problemset/task/2220/ [solution: https://cses.fi/problemset/result/6643437/]
 //Problem3 : https://atcoder.jp/contests/dp/tasks/dp_s 
 //Problem4 : https://leetcode.com/problems/count-of-integers 
 //Problem5: (digit dp with bitmask): https://leetcode.com/problems/count-special-integers

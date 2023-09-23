29969e95a56f8fa2301280fc5af69f08322cef24
Nilesh Kumar <nilesh8757@gmail.com>
2023-09-23 16:54:35 +0530

    Hashing.cpp

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,72 +1,37 @@
-//problem : https://codeforces.com/contest/271/problem/D
 
-#include<bits/stdc++.h>
- using namespace std;
-#define int long long
+problem1: https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/
+int countDistinctSubstrings(string s) { // O(n^2), n = string size
+        int n = s.size();
+        const int p = 31;
+        const int m = 1e9 + 7;
+        vector<long> p_pow(n);
 
-string bad,str;
-int k;
+        p_pow[0] = 1;
+        vector<long> hash(n + 1, 0);
 
-//first problem using rolling hash :)
-
-int count_unique_good_substrings(string const& s) 
-{
-  //source:https://cp-algorithms.com/string/string-hashing.html
-
-    int n = s.size();
-
-    const int p = 31;
-    const int m = 1e9 + 9;
-    vector<int> p_pow(n),good(n+1, 0);
-
-    p_pow[0] = 1;
-    for (int i = 1; i < n; i++)
-        p_pow[i] = (p_pow[i-1] * p) % m;
-
-    for(int i = 0; i < n; i++)
-    {
-      if(bad[s[i] - 'a'] == '0')
-        good[i + 1]++;
-    
-      good[i + 1] += good[i];
-    }
-    
-
-    vector<int> h(n + 1, 0);
-    for (int i = 0; i < n; i++)
-        h[i+1] = (h[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
-
-    int cnt = 0;
-    set<int> hs;
-    
-    for (int l = 1; l <= n; l++) 
-    {
-        hs.clear();
-       
-        for (int i = 0; i <= n - l; i++) 
-        { 
-            if(good[i + l] - good[i] > k) continue;
-
-            int cur_h = (h[i + l] + m - h[i]) % m;
-            cur_h = (cur_h * p_pow[n-i-1]) % m;
-            hs.insert(cur_h);
+        for (int i = 0; i < n; i++){
+            if(i > 0)
+              p_pow[i] = (p_pow[i-1] * p) % m;
+            hash[i+1] = (hash[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
+        }
+        int unique_substrings = 0;
+        unordered_set<int> hash_set;
+        
+        for (int len = 1; len <= n; len++) {
+            hash_set.clear();
+            for (int i = 0; i <= n - len; i++) { 
+                long cur_h = (hash[i + len] + m - hash[i]) % m;
+                cur_h = (cur_h * p_pow[n - i - 1]) % m;
+                hash_set.insert((int)cur_h);
+            }
+            unique_substrings += hash_set.size();
         }
         
-        cnt += hs.size();
+        return unique_substrings;
     }
-    
-    return cnt;
-}
+};
 
-int32_t main()
-{ 
-  
- cin >> str >> bad >> k;
-
- cout<<count_unique_good_substrings(str);
-
- return 0;
-}
 
 // problem 2 : https://codeforces.com/contest/514/problem/C [changing a char of string and looking for it in dictonary]
 // solution 2 : https://codeforces.com/contest/514/submission/61571617
+// problem 3 : //problem : https://codeforces.com/contest/271/problem/D 

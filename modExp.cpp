6bf54437253089f9e8329bb61be4d5d15efd5d6d
Nilesh Kumar <nilesh8757@gmail.com>
2023-11-15 11:21:31 +0530

    Added recursive version

--- modExp.cpp
+++ modExp.cpp
@@ -3,12 +3,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int modexp(int base, int exp,int M)
-{
+// recursive
+long long modPower(long long base, long long power, long long mod) {
+    if (power == 0)
+        return 1;
+    long long res = modPower(base, power / 2);
+    if (power % 2)
+        return (res * res * base) % mod;
+    else
+        return (res * res) % mod;
+}
+
+// iterative
+int modexp(int base, int exp,int M) {
     //(base ^ exp ) % M
     int res = 1LL;
-    while(exp) 
-    {
+    while(exp) {
        if(exp & 1) 
            res = (res * base) % M;
        base = (base * base) % M;
@@ -18,16 +28,15 @@ int modexp(int base, int exp,int M)
 }
 
 //modular inverse
-int modInverse(int a, int m) 
-{ 
+int modInverse(int a, int m) { 
  // (a ^ -1) % m, m must be prime
  return modexp(a, m-2, m);   
 } 
 
-int main() 
-{
+int main() {
     int n, tem, ans;
     cin >> n;
+    const int MOD = 1e9 + 7;
     tem = fast_exp(3, n, MOD - 1);
     ans = fast_exp(3, tem, MOD);
     cout << ans;

915f23df62ab7c783d0a942667d20cd0ddef11f5
Nilesh Kumar <nilesh8757@gmail.com>
2023-11-20 09:53:12 +0530

    Formatted

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -4,66 +4,57 @@
 */
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e5+5; 
+
+
+const int N = 1e5 + 5; 
 int n; 
-int a[N],tree[2 * N];
+int a[N], tree[2 * N];
 
-void build(int node,int start,int end)
-{
-  if(start==end)
-  {
-    tree[node]=a[start];
-  }
-  else
-  {
-    int mid=(start+end)/2;
-    build(2*node,start,mid);
-    build(2*node+1,mid+1,end);
+// #define left(n) (2 * n)
+// #define right(n) (2 * n + 1)
 
-    tree[node]=tree[2*node] + tree[2*node+1];
+void build(int node, int start, int end) {
+  if(start ==e nd) {
+    tree[node] = a[start];
+  } else {
+    int mid = (start + end) / 2;
+    build(2 * node, start, mid);
+    build(2 * node + 1, mid + 1, end);
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
   }
 }
 
-void update(int node,int start,int end,int idx,int val)
-{
-  if(start==end)
-  {
-    a[idx]=val;
-    tree[node]=val;
-  }
-  else
-  {
-    int mid=(start+end)/2;
-    if(start<=idx && idx<=mid)
-    {
-      update(2*node,start,mid,idx,val);
+void update(int node, int start, int end, int idx, int val) {
+  if(start == end) {
+    a[idx] = val;
+    tree[node] = val;
+  } else {
+    int mid = (start + end) / 2;
+    if(start <= idx && idx <= mid){
+      update(2 * node, start, mid, idx, val);
+    } else {
+      update(2 * node + 1, mid + 1, end, idx, val);
     }
-    else
-    {
-      update(2*node+1,mid+1,end,idx,val);
-    }
-
-    tree[node]=tree[2*node] + tree[2*node+1];
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
   }
 }
 
-int query(int node,int start,int end,int l,int r)
-{
-  if(r<start || end<l)
-  {
+int query(int node, int start, int end, int l, int r) {
+  if(r < start || end < l) {
     return 0;
   }
-  if(l<=start && end<=r)
-  {
+  if(l <= start && end <= r) {
     return tree[node];
   }
 
-  int mid=(start+end)/2;
-  int leftTreeSum=query(2*node,start,mid,l,r);
-  int rightTreeSum=query(2*node+1,mid+1,end,l,r);
+  int mid = (start + end) / 2;
+  int fromLeft = query(2 * node, start, mid, l, r);
+  int fromRight = query(2 * node + 1, mid + 1, end, l, r);
 
-  return(leftTreeSum+rightTreeSum);
+  return(fromLeft + fromRight);
 }
+
+
 
 int main() 
 {
